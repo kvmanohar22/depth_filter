f540ee86f63d4225dedc9f5d5c8a9dceb8721f7b
kvmanohar22 <kvmanohar22@gmail.com>
2019-08-11 18:06:46 +0530

    fix xy

--- test/test_analysis.cpp
+++ test/test_analysis.cpp
@@ -75,14 +75,14 @@ DepthAnalysis::DepthAnalysis() {
 
 void DepthAnalysis::run_two_view() {
   string img_ref_file = root_dir_+"/img/img0001_0.png"; 
-  string img_cur_file = root_dir_+"/img/img0002_0.png"; 
+  string img_cur_file = root_dir_+"/img/img0005_0.png"; 
 
   cv::Mat img_ref = cv::imread(img_ref_file); 
   cv::Mat img_cur = cv::imread(img_cur_file); 
  
   cv::Mat depth_img_ref, depth_img_cur; 
   string depth_ref_file=root_dir_+"/depth/img0001_0.depth";
-  string depth_cur_file=root_dir_+"/depth/img0002_0.depth";
+  string depth_cur_file=root_dir_+"/depth/img0005_0.depth";
   utils::load_blender_depth(depth_ref_file, *camera_, depth_img_ref); 
   utils::load_blender_depth(depth_cur_file, *camera_, depth_img_cur); 
 
@@ -93,20 +93,18 @@ void DepthAnalysis::run_two_view() {
   orb->detectAndCompute(img_ref, cv::noArray(), kps_ref, des_ref); 
 
 #ifdef DEBUG_YES
-  {
   auto temp_img = img_ref.clone();
   std::for_each(kps_ref.begin(), kps_ref.end(), [&](cv::KeyPoint &kpt) {
     cv::circle(temp_img, kpt.pt, 2, cv::Scalar(255, 0, 0));
   });
   cv::imshow("img_ref", temp_img);
-  cv::waitKey(0);
-  }
+  cv::waitKey(3000);
 #endif
 
   // load ground truth poses
   Sophus::SE3 T_w_ref, T_w_cur, T_cur_ref;
   load_pose(0, T_w_ref);
-  load_pose(1, T_w_cur);
+  load_pose(4, T_w_cur);
   T_cur_ref = T_w_cur.inverse() * T_w_ref;
 
   srand(time(0));
@@ -114,22 +112,34 @@ void DepthAnalysis::run_two_view() {
   auto kpt = kps_ref[rand_idx].pt;
   auto d = depth_img_ref.at<float>(kpt.y, kpt.x);
 
-  double dz = 1e-1;
+  double dz = 1.0;
   double d_min = 0.1*d;
   double d_max = 10*d;
-  Vector3d bearing_vec_ref(kpt.y, kpt.x, 1.0);
-  bearing_vec_ref = bearing_vec_ref / bearing_vec_ref.norm();
+  Vector2d px(kpt.x, kpt.y);
+  Vector3d bearing_vec_ref = camera_->cam2world(px);
+
+#ifdef DEBUG_YES
+  auto t_img_ref = img_ref.clone();
+  auto t_img_cur = img_cur.clone();
+  Vector3d true_xyz = bearing_vec_ref*d;
+  auto px_cur = camera_->world2cam(T_cur_ref * true_xyz);
+  cv::circle(t_img_ref, kpt, 1, cv::Scalar(255, 0, 0), CV_AA);
+  cv::circle(t_img_cur, cv::Point2f(px_cur.x(), px_cur.y()), 1, cv::Scalar(255, 0, 0), CV_AA);
+  cv::imshow("img_ref [true depth]", t_img_ref);
+  cv::imshow("img_cur [true depth]", t_img_cur);
+  cv::waitKey(3000);
+#endif
+
   while (d_min < d_max) {
     cout << "original depth = " << d << "\td_max = " << d_max << "\t" << "d_cur = " << d_min << endl;
-    auto pt_ref = bearing_vec_ref * d_min;
+    Vector3d pt_ref = bearing_vec_ref * d_min;
     Vector3d pt_cur = T_cur_ref * pt_ref;
     Vector2d uv_cur = camera_->world2cam(pt_cur);
-    cout << uv_cur.transpose() << endl;
 
     cv::Mat img_ref_n = img_ref.clone();
     cv::Mat img_cur_n = img_cur.clone();
-    cv::circle(img_ref_n, kps_ref[rand_idx].pt, 10, cv::Scalar(255, 0, 0));
-    cv::circle(img_cur_n, cv::Point2f(uv_cur.x(), uv_cur.y()), 10, cv::Scalar(0, 255, 0));
+    cv::circle(img_ref_n, kps_ref[rand_idx].pt, 2, cv::Scalar(255, 0, 0), CV_AA);
+    cv::circle(img_cur_n, cv::Point2f(uv_cur.x(), uv_cur.y()), 2, cv::Scalar(0, 255, 0), CV_AA);
 
     cv::imshow("img_ref", img_ref_n);
     cv::imshow("img_cur", img_cur_n);
