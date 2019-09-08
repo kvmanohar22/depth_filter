580bf775d3e1418727f605fb2dad6e51d042159f
kvmanohar22 <kvmanohar22@gmail.com>
2019-09-09 04:25:33 +0530

    these mistakes huh?

--- include/depth_filter/similarity.h
+++ include/depth_filter/similarity.h
@@ -9,33 +9,50 @@ namespace similarity {
 template <typename DTYPE, size_t N>
 class Abstract {
 public:
+  Abstract()
+    : half_patch_size_(N),
+      patch_area_(2*N*2*N),
+      ref_patch_(nullptr)
+  {}
+
   Abstract(DTYPE* ref_patch)
     : half_patch_size_(N),
-      patch_size_(N*N),
+      patch_area_(2*N*2*N),
       ref_patch_(ref_patch),
       sumR_(0),
       sumR2_(0)
   {
-    for(size_t y=0; y<half_patch_size_; ++y) {
-      for(size_t x=0; x<half_patch_size_; ++x) {
-        DTYPE val = *(ref_patch_+y*half_patch_size_+x);
+    if (ref_patch_!=nullptr)
+      recompute();
+  }
+  virtual ~Abstract() =default;
+  virtual double similarity(DTYPE* new_patch, size_t cols) const =0;
+  inline const size_t half_patch_size() const { return half_patch_size_; }
+  inline const size_t patch_area() const      { return patch_area_; }
+  inline const double sumR() const { return sumR_; }
+  inline const double sumR2() const { return sumR2_; }
+  inline const DTYPE* ref_patch() const { return ref_patch_; }
+  inline void set_ref_patch(DTYPE* ref_patch) {
+    ref_patch_ = ref_patch;
+    recompute();
+  }
+  inline void recompute() {
+    sumR_ = 0;
+    sumR2_ = 0;
+    size_t patch_size = 2*half_patch_size_;
+    for(size_t y=0; y<patch_size; ++y) {
+      for(size_t x=0; x<patch_size; ++x) {
+        DTYPE val = *(ref_patch_+y*patch_size+x);
         sumR_ += val;
         sumR2_ += val*val;
       }
     }
   }
-  virtual ~Abstract() =default;
-  virtual double similarity(DTYPE* new_patch, size_t cols) const =0;
-  inline const size_t half_patch_size() const { return half_patch_size_; }
-  inline const size_t patch_size() const      { return patch_size_; }
-  inline const double sumR() const { return sumR_; }
-  inline const double sumR2() const { return sumR2_; }
-  inline const DTYPE* ref_patch() const { return ref_patch_; }
 
 protected:
-  const DTYPE* ref_patch_; // reference patch
-  size_t half_patch_size_; // rows or cols
-  size_t patch_size_;      // rows*cols
+  DTYPE* ref_patch_;       // reference patch
+  size_t half_patch_size_; // rows/2 or cols/2
+  size_t patch_area_;      // rows*cols
   double sumR_;            // sum of all pixels
   double sumR2_;           // sum of squares of all pixels
 };
@@ -44,17 +61,18 @@ protected:
 template <typename DTYPE, size_t N>
 class SSD : public Abstract<DTYPE, N> {
 public:
+  SSD() : Abstract<DTYPE, N>() {}
   SSD(DTYPE* ref_patch_) : Abstract<DTYPE, N>(ref_patch_) {}
   virtual ~SSD() =default;
   double similarity(DTYPE* new_patch, size_t cols) const override {
     double sumC2=0, sumRC=0;
     const DTYPE* ref_patch=Abstract<DTYPE, N>::ref_patch();
-    const size_t half_size = Abstract<DTYPE, N>::half_patch_size();
-    for(size_t y=0; y<half_size; ++y) {
-      for(size_t x=0; x<half_size; ++x) {
+    const size_t patch_size = 2*Abstract<DTYPE, N>::half_patch_size();
+    for(size_t y=0; y<patch_size; ++y) {
+      for(size_t x=0; x<patch_size; ++x) {
         DTYPE val = *(new_patch+y*cols+x);
         sumC2 += val*val;
-        sumRC += val*ref_patch[y*half_size+x];
+        sumRC += val*ref_patch[y*patch_size+x];
       }
     }
     const double sR2=Abstract<DTYPE, N>::sumR2();
@@ -66,22 +84,23 @@ public:
 template <typename DTYPE, size_t N>
 class ZMSSD : public Abstract<DTYPE, N> {
 public:
+  ZMSSD() : Abstract<DTYPE, N>() {}
   ZMSSD(DTYPE* ref_patch_) : Abstract<DTYPE, N>(ref_patch_) {}
   virtual ~ZMSSD() =default;
   double similarity(DTYPE* new_patch, size_t cols) const override {
     double sumC=0, sumC2=0, sumRC=0;
     const DTYPE* ref_patch=Abstract<DTYPE, N>::ref_patch();
-    const size_t half_size = Abstract<DTYPE, N>::half_patch_size();
-    for(size_t y=0; y<half_size; ++y) {
-      for(size_t x=0; x<half_size; ++x) {
+    const size_t patch_size = 2*Abstract<DTYPE, N>::half_patch_size();
+    for(size_t y=0; y<patch_size; ++y) {
+      for(size_t x=0; x<patch_size; ++x) {
         DTYPE val = *(new_patch+y*cols+x);
         sumC += val;
         sumC2 += val*val;
-        sumRC += val*ref_patch[y*half_size+x];
+        sumRC += val*ref_patch[y*patch_size+x];
       }
     }
     const double sR=Abstract<DTYPE, N>::sumR(), sR2=Abstract<DTYPE, N>::sumR2();
-    return sR2+sumC2-2*sumRC-(sR*sR+sumC*sumC-2*sR*sumC)/(half_size*half_size);
+    return sR2+sumC2-2*sumRC-(sR*sR+sumC*sumC-2*sR*sumC)/(patch_size*patch_size);
   }
 };
 
@@ -89,17 +108,18 @@ public:
 template <typename DTYPE, size_t N>
 class NCC : public Abstract<DTYPE, N> {
 public:
+  NCC() : Abstract<DTYPE, N>() {}
   NCC(DTYPE* ref_patch_) : Abstract<DTYPE, N>(ref_patch_) {} 
   virtual ~NCC() =default;
   double similarity(DTYPE* new_patch, size_t cols) const override {
     double sumC2=0, sumRC=0;
     const DTYPE* ref_patch=Abstract<DTYPE, N>::ref_patch();
-    const size_t half_size = Abstract<DTYPE, N>::half_patch_size();
-    for(size_t y=0; y<half_size; ++y) {
-      for(size_t x=0; x<half_size; ++x) {
+    const size_t patch_size = 2*Abstract<DTYPE, N>::half_patch_size();
+    for(size_t y=0; y<patch_size; ++y) {
+      for(size_t x=0; x<patch_size; ++x) {
         DTYPE val = *(new_patch+y*cols+x);
         sumC2 += val*val;
-        sumRC += val*ref_patch[y*half_size+x];
+        sumRC += val*ref_patch[y*patch_size+x];
       }
     }
     const double sR2=Abstract<DTYPE, N>::sumR2();
@@ -111,23 +131,24 @@ public:
 template <typename DTYPE, size_t N>
 class ZMNCC : public Abstract<DTYPE, N> {
 public:
+  ZMNCC() : Abstract<DTYPE, N>() {}
   ZMNCC(DTYPE* ref_patch_) : Abstract<DTYPE, N>(ref_patch_) {}
   virtual ~ZMNCC() =default;
   double similarity(DTYPE* new_patch, size_t cols) const override {
     double sumC=0, sumC2=0, sumRC=0;
     const DTYPE* ref_patch=Abstract<DTYPE, N>::ref_patch();
-    const size_t half_size = Abstract<DTYPE, N>::half_patch_size();
-    for(size_t y=0; y<half_size; ++y) {
-      for(size_t x=0; x<half_size; ++x) {
+    const size_t patch_size = 2*Abstract<DTYPE, N>::half_patch_size();
+    for(size_t y=0; y<patch_size; ++y) {
+      for(size_t x=0; x<patch_size; ++x) {
         DTYPE val = *(new_patch+y*cols+x);
         sumC += val;
         sumC2 += val*val;
-        sumRC += val*ref_patch[y*half_size+x];
+        sumRC += val*ref_patch[y*patch_size+x];
       }
     }
-    const size_t full_size = Abstract<DTYPE, N>::patch_size();
+    const size_t full_area = Abstract<DTYPE, N>::patch_area();
     const double sR=Abstract<DTYPE, N>::sumR(), sR2=Abstract<DTYPE, N>::sumR2();
-    return (sumRC-sR*sumC/full_size)/sqrt((sR2-sR*sR/full_size)*(sumC2-sumC*sumC/full_size));
+    return (sumRC-sR*sumC/full_area)/sqrt((sR2-sR*sR/full_area)*(sumC2-sumC*sumC/full_area));
   }
 };
 
--- test/test_similarity.cpp
+++ test/test_similarity.cpp
@@ -7,26 +7,26 @@ void test1() {
   uint8_t ref_patch1_[] = {1, 2, 3, 4};
   uint8_t cur_patch1_[] = {1, 2, 3, 4};
  
-  static constexpr size_t half_patch_size = 4; 
-  uint8_t ref_patch_[] = {76, 72, 82, 77, 68, 68, 79, 90, 77, 81, 84, 71, 72, 70};
-  uint8_t cur_patch_[] = {76, 72, 82, 77, 68, 68, 79, 90, 77, 81, 84, 71, 72, 70};
+  static constexpr size_t half_patch_size = 2;
+  uint8_t ref_patch_[] = {76, 72, 82, 77, 68, 68, 79, 90, 77, 81, 84, 71, 72, 70, 72, 70};
+  uint8_t cur_patch_[] = {76, 72, 82, 77, 68, 68, 79, 90, 77, 81, 84, 71, 72, 70, 72, 70};
   
   utils::similarity::ZMSSD<uint8_t, half_patch_size> ssd_uint8_t(ref_patch_);
-  cout << "ssd similarity = " << ssd_uint8_t.similarity(cur_patch_, half_patch_size) << endl;
+  cout << "ssd similarity = " << ssd_uint8_t.similarity(cur_patch_, 2*half_patch_size) << endl;
 
   utils::similarity::ZMSSD<uint8_t, half_patch_size> zmssd_uint8_t(ref_patch_);
-  cout << "zmssd similarity = " << zmssd_uint8_t.similarity(cur_patch_, half_patch_size) << endl;
+  cout << "zmssd similarity = " << zmssd_uint8_t.similarity(cur_patch_, 2*half_patch_size) << endl;
 
   utils::similarity::NCC<uint8_t, half_patch_size> ncc_uint8_t(ref_patch_);
-  cout << "ncc similarity = " << ncc_uint8_t.similarity(cur_patch_, half_patch_size) << endl;
+  cout << "ncc similarity = " << ncc_uint8_t.similarity(cur_patch_, 2*half_patch_size) << endl;
 
   utils::similarity::ZMNCC<uint8_t, half_patch_size> zmncc_uint8_t(ref_patch_);
-  cout << "zmncc similarity = " << zmncc_uint8_t.similarity(cur_patch_, half_patch_size) << endl;
+  cout << "zmncc similarity = " << zmncc_uint8_t.similarity(cur_patch_, 2*half_patch_size) << endl;
 }
 
 void test2() {
   static constexpr size_t half_patch_size = 4;
-  size_t patch_size = half_patch_size*half_patch_size;
+  size_t patch_size = 2*half_patch_size;
   size_t start_idx_x = 260;
   size_t start_idx_y = 270;
  
@@ -39,11 +39,11 @@ void test2() {
 
   // create reference patch
   uint8_t* img_ptr = img.ptr<uint8_t>();
-  uint8_t  ref_patch[patch_size] __attribute__ ((aligned(16)));
-  uint8_t  cur_patch[patch_size] __attribute__ ((aligned(16)));
-  for(size_t y=0;y<half_patch_size;++y)
-    for(size_t x=0;x<half_patch_size;++x)
-      *(ref_patch+y*half_patch_size+x) = *(img_ptr+(start_idx_y+y)*img.cols+start_idx_x+x);  
+  uint8_t  ref_patch[patch_size*patch_size] __attribute__ ((aligned(16)));
+  uint8_t  cur_patch[patch_size*patch_size] __attribute__ ((aligned(16)));
+  for(size_t y=0;y<patch_size;++y)
+    for(size_t x=0;x<patch_size;++x)
+      *(ref_patch+y*patch_size+x) = *(img_ptr+(start_idx_y+y)*img.cols+start_idx_x+x);  
 
   std::ofstream ofile(base+"/data/zmssd_similarity.txt");
   utils::similarity::ZMSSD<uint8_t, half_patch_size> zmssd(ref_patch);
@@ -64,7 +64,107 @@ void test2() {
 
 }
 
+void test3() {
+  static constexpr size_t half_patch_size = 4;
+  size_t patch_size = 2*half_patch_size;
+  size_t start_idx_x = 272;
+  size_t start_idx_y = 348;
+ 
+  string base = std::getenv("PROJECT_DF");
+  string imgfile = base + "/data/test_image.png";
+  cv::Mat img = cv::imread(imgfile.c_str(), 0);
+  assert(!img.empty());
+  cv::imshow("img", img);
+  cv::waitKey(0);
+
+  // create reference patch
+  uint8_t* img_ptr = img.ptr<uint8_t>();
+  uint8_t  ref_patch[patch_size*patch_size] __attribute__ ((aligned(16)));
+  uint8_t  cur_patch[patch_size*patch_size] __attribute__ ((aligned(16)));
+  for(size_t y=0;y<patch_size;++y)
+    for(size_t x=0;x<patch_size;++x)
+      *(ref_patch+y*patch_size+x) = *(img_ptr+(start_idx_y+y)*img.cols+start_idx_x+x);  
+
+  utils::similarity::ZMNCC<uint8_t, half_patch_size> zmncc(ref_patch);
+
+  // ref patch 1
+  std::ofstream ofile;
+  ofile.open(base+"/data/zmncc_test3_similarity.txt");
+  for(size_t x=0; x<img.cols; x+=half_patch_size) {
+    uint8_t* cur_patch_ptr = img_ptr+start_idx_y*img.cols+x;
+    double similarity = zmncc.similarity(cur_patch_ptr, img.cols);
+    ofile << x << " "  << similarity << endl;
+  }
+  ofile.close();
+
+  // ref patch 2
+  start_idx_x = 260;
+  start_idx_y = 270;
+  for(size_t y=0;y<patch_size;++y)
+    for(size_t x=0;x<patch_size;++x)
+      *(ref_patch+y*patch_size+x) = *(img_ptr+(start_idx_y+y)*img.cols+start_idx_x+x);  
+  zmncc.set_ref_patch(ref_patch);
+  ofile.open(base+"/data/zmncc_test3_similarity2.txt");
+  for(size_t x=0; x<img.cols; x+=half_patch_size) {
+    uint8_t* cur_patch_ptr = img_ptr+start_idx_y*img.cols+x;
+    double similarity = zmncc.similarity(cur_patch_ptr, img.cols);
+    ofile << x << " "  << similarity << endl;
+  }
+  ofile.close();
+
+  // ref patch 1
+  start_idx_x = 272;
+  start_idx_y = 348;
+  for(size_t y=0;y<patch_size;++y)
+    for(size_t x=0;x<patch_size;++x)
+      *(ref_patch+y*patch_size+x) = *(img_ptr+(start_idx_y+y)*img.cols+start_idx_x+x);  
+  zmncc.set_ref_patch(ref_patch);
+  ofile.open(base+"/data/zmncc_test3_similarity3.txt");
+  for(size_t x=0; x<img.cols; x+=half_patch_size) {
+    uint8_t* cur_patch_ptr = img_ptr+start_idx_y*img.cols+x;
+    double similarity = zmncc.similarity(cur_patch_ptr, img.cols);
+    ofile << x << " "  << similarity << endl;
+  }
+}
+
+void test4() {
+  static constexpr size_t half_patch_size = 4;
+  size_t patch_size = 2*half_patch_size;
+  size_t start_idx_x = 272;
+  size_t start_idx_y = 348;
+ 
+  string base = std::getenv("PROJECT_DF");
+  string imgfile = base + "/data/test_image.png";
+  cv::Mat img = cv::imread(imgfile.c_str(), 0);
+  assert(!img.empty());
+  cv::imshow("img", img);
+  cv::waitKey(0);
+
+  // create reference patch
+  uint8_t* img_ptr = img.ptr<uint8_t>();
+  uint8_t  ref_patch[patch_size*patch_size] __attribute__ ((aligned(16)));
+  uint8_t  cur_patch[patch_size*patch_size] __attribute__ ((aligned(16)));
+  for(size_t y=0;y<patch_size;++y)
+    for(size_t x=0;x<patch_size;++x)
+      *(ref_patch+y*patch_size+x) = *(img_ptr+(start_idx_y+y)*img.cols+start_idx_x+x);  
+
+  utils::similarity::ZMNCC<uint8_t, half_patch_size> zmncc;
+  zmncc.set_ref_patch(ref_patch);
+
+  // ref patch 1
+  std::ofstream ofile;
+  ofile.open(base+"/data/zmncc_test4_similarity.txt");
+  for(size_t x=0; x<img.cols; x+=half_patch_size) {
+    uint8_t* cur_patch_ptr = img_ptr+start_idx_y*img.cols+x;
+    double similarity = zmncc.similarity(cur_patch_ptr, img.cols);
+    ofile << x << " "  << similarity << endl;
+  }
+  ofile.close();
+}
+
 int main() {
   test1();
   test2();
+  test3();
+  test4();
 }
